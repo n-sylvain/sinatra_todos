fb9cb95fbcd5baf0234718166f391fab4709bbd8
Sylvain Ni <sylvainni@msn.com>
2024-04-30 11:25:58 +0200

    Adding new list feature

--- todo.rb
+++ todo.rb
@@ -2,16 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
+  @lists = session[:lists]
+  # [
+  #   {name: "Lunch Groceries", todos: []},
+  #   {name: "Dinner Groceries", todos: []}
+  # ]
   erb :lists, layout: :layout
 end
 
 set :session_secret, SecureRandom.hex(32)
+
+get "/lists/new" do
+  session[:lists] << { name: "New List", todos: [] }
+  redirect "/lists"
+end
