b9e7c3ca2ba8c6f36eaaf2d009df10a5eed7ce17
Sylvain Ni <sylvainni@msn.com>
2024-04-30 11:36:09 +0200

    Adding adding new list

--- todo.rb
+++ todo.rb
@@ -16,17 +16,24 @@ get "/" do
 end
 
 get "/lists" do
-  @lists = session[:lists]
   # [
   #   {name: "Lunch Groceries", todos: []},
   #   {name: "Dinner Groceries", todos: []}
   # ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 set :session_secret, SecureRandom.hex(32)
 
 get "/lists/new" do
-  session[:lists] << { name: "New List", todos: [] }
+  # session[:lists] << { name: "New List", todos: [] }
+  # redirect "/lists"
+  erb :new_list, layout: :layout
+
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
   redirect "/lists"
 end
