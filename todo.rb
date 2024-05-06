a8f8ca1424297aa762e266061b9fec7fae5f8402
Sylvain Ni <sylvainni@msn.com>
2024-05-06 13:35:29 +0200

    Reflowing validation checks

--- todo.rb
+++ todo.rb
@@ -46,12 +46,15 @@ end
 post "/lists" do
   list_name = params[:list_name].strip
   # if list_name.size >= 1 && list_name.size <= 100
-  if (1..100).cover? list_name.size
-    session[:lists] << {name: params[:list_name], todos: []}
-    session[:success] = "The list has been created."
-    redirect "/lists"
-  else
+  if !(1..100).cover? list_name.size
     session[:error] = "List name must be between 1 and 100 characters."
     erb :new_list, layout: :layout
+  elsif session[:lists].any? { |list| list[:name] == list_name }
+    session[:error] = "List name must be unique."
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
   end
 end
